f99ed2d8fc150c49002457e78ce1998307a87d44
Adrian Olmo <47026705+adrian-olmo@users.noreply.github.com>
2021-06-10 21:43:07 +0200

    Diseño + Func Partidos // Creacion de equipos

---
+++ stats-app/src/containers/createTeam/CreateTeam.js
@@ -0,0 +1,22 @@
+import React, { useEffect, useState } from 'react';
+import { useHistory } from 'react-router';
+import './CreateTeam.scss';
+import { FormTeam } from "../../components/formTeam/FormTeam";
+import { fetchCreateTeam } from '../../services/fetchCreateTeam';
+
+export const CreateTeam = (props) => {
+
+    let history = useHistory();
+
+    const createTeam = async (name, confederation, manager, fifa_rank, total_titles, logo) => {
+        const result = await fetchCreateTeam(name, confederation, manager, fifa_rank, total_titles, logo);
+        history.push('/teams')
+
+    }
+
+    return (
+        <div className="app-body">
+            <FormTeam typeCrudAction="CREATE" submitFunction={createTeam} message="Crea los datos del Equipo" />
+        </div>
+    )
+}
